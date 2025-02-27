b31fa301cd10fa79909d598faa7c16a1e8336f24
YU-7 <58320198+YU-7@users.noreply.github.com>
2025-02-27 11:41:20 +0800

    Add -> 新增home页面

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,31 @@
-function Home() {
-  return (
-    <div></div>
-  );
-}
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
+import { Search } from "lucide-react"
+import CollapsibleSidebar from "@/components/CollapsibleSidebar";
 
-export default Home;
+export default function Home() {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      {/* 左侧导航栏 */}
+    <CollapsibleSidebar />
+
+      {/* 右侧内容区域 */}
+      <div className="flex-1 p-8 overflow-y-auto">
+        <div className="space-y-6">
+          {/* 卡片列表 */}
+          {[1, 2, 3, 4, 5].map((item) => (
+            <Card key={item}>
+              <CardHeader>
+                <CardTitle>卡片标题 {item}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>卡片内容，这里可以放置相关描述信息...</p>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
