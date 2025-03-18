c6eec96c0b363f2996ea6fcd7bb79fc4c3884dc7
YU-7 <2747046473@qq.com>
2025-03-18 20:52:38 +0800

    Mod -> 条件编译

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,3 @@
-use tauri_plugin_autostart::MacosLauncher;
-
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,15 +8,21 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_autostart::init(
-            MacosLauncher::AppleScript,
-            None,
-        ))
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_sql::Builder::default().build())
+        // .setup(move |app| {
+        //     #[cfg(desktop)]
+        //     {
+        //         app.handle().plugin(tauri_plugin_autostart::init(
+        //             tauri_plugin_autostart::MacosLauncher::AppleScript,
+        //             None,
+        //         ))?;
+        //     }
+        //     Ok(()) // 返回一个 Ok(()) 以满足 Result<(), Box<dyn Error>> 类型要求
+        // })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
